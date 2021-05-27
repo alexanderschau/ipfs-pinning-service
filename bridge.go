28f7548ffcfaa1808d852b9a90be54a0a223a3f4
Alexander Schau <alexander@schau.email>
2021-05-27 13:27:21 +0200

    add: response type

--- bridge.go
+++ bridge.go
@@ -24,33 +24,43 @@ func NewPinsApiController(s openapi.PinsApiServicer) openapi.Router {
 }
 
 // ErrorResponse - return this on failure
-func ErrorResponse(statusCode int, details string) (openapi.ImplResponse, error) {
-	return openapi.Response(statusCode, openapi.Failure{
+func ErrorResponse(statusCode int, details string) Response {
+	resp := openapi.Response(statusCode, openapi.Failure{
 		Error: openapi.FailureError{
 			Reason:  http.StatusText(statusCode),
 			Details: details,
 		},
-	}), nil
+	})
+	return Response{resp, nil}
 }
 
 // PinCreationStatusResponse - return this, if a new pin was added
-func PinCreationStatusResponse(pinStatus openapi.PinStatus) (openapi.ImplResponse, error) {
-	return openapi.Response(http.StatusCreated, pinStatus), nil
+func PinCreationStatusResponse(pinStatus openapi.PinStatus) Response {
+	resp := openapi.Response(http.StatusCreated, pinStatus)
+	return Response{resp, nil}
 }
 
 // PinStatusResponse - return this on PinsRequestidGet
-func PinStatusResponse(pinStatus openapi.PinStatus) (openapi.ImplResponse, error) {
-	return openapi.Response(http.StatusOK, pinStatus), nil
+func PinStatusResponse(pinStatus openapi.PinStatus) Response {
+	resp := openapi.Response(http.StatusOK, pinStatus)
+	return Response{resp, nil}
 }
 
 // DeleteResponse - return this on PinsRequestidDelete
-func DeleteResponse() (openapi.ImplResponse, error) {
-	return openapi.Response(http.StatusOK, nil), nil
+func DeleteResponse() Response {
+	resp := openapi.Response(http.StatusOK, nil)
+	return Response{resp, nil}
 }
 
 // PinListResponse - return a list of pins (used in PinsGet)
-func PinListResponse(pinResults openapi.PinResults) (openapi.ImplResponse, error) {
-	return openapi.Response(http.StatusOK, pinResults), nil
+func PinListResponse(pinResults openapi.PinResults) Response {
+	resp := openapi.Response(http.StatusOK, pinResults)
+	return Response{resp, nil}
+}
+
+type Response struct {
+	Resp openapi.ImplResponse
+	Err  error
 }
 
 type PinsGetInputData struct {
@@ -88,15 +98,15 @@ type PinsRequestidPostInputData struct {
 
 type Service struct {
 	// PinsGet - List pin objects
-	PinsGet func(PinsGetInputData) (openapi.ImplResponse, error)
+	PinsGet func(PinsGetInputData) Response
 	// PinsPost - Add pin object
-	PinsPost func(PinsPostInputData) (openapi.ImplResponse, error)
+	PinsPost func(PinsPostInputData) Response
 	// PinsRequestidDelete - Remove pin object
-	PinsRequestidDelete func(PinsRequestidDeleteInputData) (openapi.ImplResponse, error)
+	PinsRequestidDelete func(PinsRequestidDeleteInputData) Response
 	// PinsRequestidGet - Get pin object
-	PinsRequestidGet func(PinsRequestidGetInputData) (openapi.ImplResponse, error)
+	PinsRequestidGet func(PinsRequestidGetInputData) Response
 	// PinsRequestidPost - Replace pin object
-	PinsRequestidPost func(PinsRequestidPostInputData) (openapi.ImplResponse, error)
+	PinsRequestidPost func(PinsRequestidPostInputData) Response
 }
 
 type PinsApiService struct {
@@ -104,7 +114,7 @@ type PinsApiService struct {
 }
 
 func (s *PinsApiService) PinsGet(ctx context.Context, cid []string, name string, match openapi.TextMatchingStrategy, status []openapi.Status, before time.Time, after time.Time, limit int32, meta map[string]string) (openapi.ImplResponse, error) {
-	return s.userFunctions.PinsGet(PinsGetInputData{
+	r := s.userFunctions.PinsGet(PinsGetInputData{
 		ctx:    ctx,
 		cid:    cid,
 		name:   name,
@@ -115,33 +125,38 @@ func (s *PinsApiService) PinsGet(ctx context.Context, cid []string, name string,
 		limit:  limit,
 		meta:   meta,
 	})
+	return r.Resp, r.Err
 }
 
 func (s *PinsApiService) PinsPost(ctx context.Context, pin openapi.Pin) (openapi.ImplResponse, error) {
-	return s.userFunctions.PinsPost(PinsPostInputData{
+	r := s.userFunctions.PinsPost(PinsPostInputData{
 		ctx: ctx,
 		pin: pin,
 	})
+	return r.Resp, r.Err
 }
 
 func (s *PinsApiService) PinsRequestidDelete(ctx context.Context, requestid string) (openapi.ImplResponse, error) {
-	return s.userFunctions.PinsRequestidDelete(PinsRequestidDeleteInputData{
+	r := s.userFunctions.PinsRequestidDelete(PinsRequestidDeleteInputData{
 		ctx:       ctx,
 		requestid: requestid,
 	})
+	return r.Resp, r.Err
 }
 
 func (s *PinsApiService) PinsRequestidGet(ctx context.Context, requestid string) (openapi.ImplResponse, error) {
-	return s.userFunctions.PinsRequestidGet(PinsRequestidGetInputData{
+	r := s.userFunctions.PinsRequestidGet(PinsRequestidGetInputData{
 		ctx:       ctx,
 		requestid: requestid,
 	})
+	return r.Resp, r.Err
 }
 
 func (s *PinsApiService) PinsRequestidPost(ctx context.Context, requestid string, pin openapi.Pin) (openapi.ImplResponse, error) {
-	return s.userFunctions.PinsRequestidPost(PinsRequestidPostInputData{
+	r := s.userFunctions.PinsRequestidPost(PinsRequestidPostInputData{
 		ctx:       ctx,
 		requestid: requestid,
 		pin:       pin,
 	})
+	return r.Resp, r.Err
 }
